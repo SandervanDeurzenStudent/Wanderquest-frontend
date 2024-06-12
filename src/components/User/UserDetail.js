ca10d3f71d5422d426979eccbaf7cc4f52b4842e
Sander van Deurzen <467376@student.fontys.nl>
2024-06-12 10:32:40 +0200

    fr

--- src/components/User/UserDetail.js
+++ src/components/User/UserDetail.js
@@ -3,7 +3,7 @@ import React from 'react';
 const UserDetail = ({ user }) => {
     return (
         <div>
-            <h2>{user.name}</h2>
+            <h2>{user.name}.</h2>
             <p>{user.email}</p>
         </div>
     );
